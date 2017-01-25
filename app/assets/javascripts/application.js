3c45f7b65182459f447f74beaeda7724a272e7ce
xieyanlin <xieyanlin820@gmail.com>
2017-01-25 17:07:50 +0800

    little change

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap/alert
+//= require bootstrap-sprockets
 //= require bootstrap/dropdown
 //= require_tree .
 //= require local_time
